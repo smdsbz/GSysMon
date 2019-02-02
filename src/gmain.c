22964084d8af113bf1344a3091127bfd636700e7
smdsbz <694066681@qq.com>
2019-02-02 23:20:20 +0800

    saving work

--- src/gmain.c
+++ src/gmain.c
@@ -8,51 +8,81 @@
 #include "../include/system.h"
 
 
-void on_search_choice_select(GtkMenuItem *item, gpointer sbar) {
-    gtk_search_bar_set_search_mode(GTK_SEARCH_BAR(sbar), TRUE);
-}
+GtkBuilder *builder;    // making it global makes things a lot more easier
 
-void on_search_toggle_clicked(GtkToolButton *btn, gpointer sbar) {
-    gtk_search_bar_set_search_mode(GTK_SEARCH_BAR(sbar), TRUE);
-}
+static void on_search_choice_activate(GtkMenuItem *item, gpointer method)
+{   // {{{
+    char sbar_name[32];
+    sprintf(sbar_name, "search-bar-%s", (char *)method);
+    gtk_search_bar_set_search_mode(
+        GTK_SEARCH_BAR(gtk_builder_get_object(builder, sbar_name)),
+        TRUE
+    );
+    if (strequ(method, "pid")) {
+        gtk_search_bar_set_search_mode(
+            GTK_SEARCH_BAR(gtk_builder_get_object(builder, "search-bar-name")),
+            FALSE
+        );
+    } else {
+        gtk_search_bar_set_search_mode(
+            GTK_SEARCH_BAR(gtk_builder_get_object(builder, "search-bar-pid")),
+            FALSE
+        );
+    }
+}   // }}}
 
-void on_destroy(GtkWidget *widget, gpointer data) {
-    g_print("on_destroy: ...\n");
+static void on_search_toggle_clicked(GtkToolButton *btn, gpointer _)
+{   // {{{
+    gtk_search_bar_set_search_mode(
+        GTK_SEARCH_BAR(gtk_builder_get_object(builder, "search-bar-name")),
+        TRUE
+    );
+    gtk_search_bar_set_search_mode(
+        GTK_SEARCH_BAR(gtk_builder_get_object(builder, "search-bar-pid")),
+        FALSE
+    );
+}   // }}}
+
+static void refresh_process_liststore(GtkListStore *ls)
+{   // {{{
+    // TODO
+}   // }}}
+
+static void on_destroy(GtkWidget *widget, gpointer _)
+{   // {{{
     gtk_main_quit();
     return;
-}
+}   // }}}
 
 
 int main(int argc, char **argv) {
 
     gtk_init(&argc, &argv);
 
-    GtkBuilder *builder = gtk_builder_new_from_file("ui/main.glade");
+    builder = gtk_builder_new_from_file("ui/main.glade");
 
     GObject *main_window = gtk_builder_get_object(builder, "main-window");
     g_signal_connect(main_window, "destroy", G_CALLBACK(on_destroy), NULL);
     gtk_window_set_default_size(GTK_WINDOW(main_window), 800, 500);
 
+    // #search-toggle
+    g_signal_connect(
+        gtk_builder_get_object(builder, "search-toggle"),
+        "clicked", G_CALLBACK(on_search_toggle_clicked), NULL
+    );
+
+    // #search-choice-(pid|name)
     gtk_menu_tool_button_set_menu(
         GTK_MENU_TOOL_BUTTON(gtk_builder_get_object(builder, "search-toggle")),
         GTK_WIDGET(gtk_builder_get_object(builder, "search-method-choice"))
     );
-
-    g_signal_connect(
-        gtk_builder_get_object(builder, "search-toggle"),
-        "clicked", G_CALLBACK(on_search_toggle_clicked),
-        gtk_builder_get_object(builder, "search-bar-name")
-    );
-
     g_signal_connect(
         gtk_builder_get_object(builder, "search-choice-pid"),
-        "activate", G_CALLBACK(on_search_choice_select),
-        gtk_builder_get_object(builder, "search-bar-pid")
+        "activate", G_CALLBACK(on_search_choice_activate), "pid"
     );
     g_signal_connect(
         gtk_builder_get_object(builder, "search-choice-name"),
-        "activate", G_CALLBACK(on_search_choice_select),
-        gtk_builder_get_object(builder, "search-bar-name")
+        "activate", G_CALLBACK(on_search_choice_activate), "name"
     );
 
     gtk_widget_show_all(GTK_WIDGET(main_window));
